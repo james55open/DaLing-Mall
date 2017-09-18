6e0f2f3bc03f173e4ba653ca76305b5ed8adf6f5
james55open <381326791@qq.com>
2017-09-18 21:55:40 +0800

    first commit

---
+++ detail/js/detail.js
@@ -0,0 +1,176 @@
+﻿window.onload = function(){
+	
+	var daling = document.getElementById('daling');
+	var daling1 = document.getElementById('daling1');
+	daling.onmouseover = function(){
+		daling1.style.display = "block";
+	}
+	daling.onmouseout = function(){
+		daling1.style.display = "none";
+	}
+	daling1.onmouseover = function(){
+		daling1.style.display = "block";
+	}
+	daling1.onmouseout = function(){
+		daling1.style.display = "none";
+	}//
+	//换图片
+	var getli = document.getElementById('lis');
+	var lis = getli.getElementsByTagName('a')
+		for(var i = 0; i < lis.length ;i++){
+			lis[i].index = i; 
+			//console.log(i);
+			lis[i].onclick = function (){  
+			var picsic = "img/" +"nav_l"+(this.index + 1) + ".png";
+			//console.log(picsic);
+			var look = document.getElementById('look');
+			look.src = picsic;
+			//console.log(look.src);
+			var bigimg = document.getElementById('b_bimg')
+			bigimg.src = picsic;
+		}
+	}
+	/*$("#shop").mouseenter(function(){
+		$("#shopshow").css("display","block");
+		$("#shop").css("display","none");
+		$("#bor").css("border","1px solid black");
+	});
+	$("#shop").mouseout(function(){
+		$("#shopshow").css("display","none");
+		$("#shop").css("display","block");
+		$("#bor").css("border","none");
+	});*/
+	
+	$("#bor").mouseover(function(){
+		$("#shopshow").css("display","block");
+		$("#shop").css("display","none");
+		$("#bor").css("border","1px solid black");
+	});
+	$("#bor").mouseout(function(){
+		$("#shopshow").css("display","none");
+		$("#shop").css("display","block");
+		$("#bor").css("border","none");
+	});
+//放大镜
+	   var box = document.getElementById('box');
+	   var simg = document.getElementById('look');
+	   var move = document.getElementById('move');
+	   var bimgDiv = document.getElementById('bimg');
+	   var bimg = document.getElementById('b_bimg');
+	   box.onmousemove = function (e){
+		   // 获取事件对象，用于获取鼠标位置
+		   var evt = e || window.event;
+		   
+		   var l = evt.clientX;
+		   var t = evt.clientY;
+		   
+			//**页面滑动《滚动放大镜效果依然存在》
+			var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+			var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+			//页面滑动
+			
+		   // 计算滑块的位置
+		   var toLeft =scrollLeft + l - box.offsetLeft - move.offsetWidth / 2;
+		   var toTop = scrollTop + t - box.offsetTop - move.offsetHeight / 2;
+		   
+		   // 最大可以到达的left
+		   var maxLeft = box.offsetWidth - move.offsetWidth;
+		   if(toLeft < 0){
+			  toLeft  = 0;
+		   } else if (toLeft > maxLeft){
+			  toLeft = maxLeft;
+		   }
+		   
+		   // 最大可以到达的top
+		   var maxTop = box.offsetHeight - move.offsetHeight ;
+		   if(toTop < 0){
+			  toTop = 0;
+		   } else if (toTop > maxTop){
+			  toTop = maxTop;
+		   }
+		   
+		   // 将滑块放到适当的位置上
+		   move.style.left = toLeft + 'px';
+		   move.style.top = toTop + 'px';
+		   
+		   // 将大图放到适当的位置
+		   var rate = 400 / 374; // 计数大图比上小图的比例
+		   // 按比例放大移动距离
+		   bimg.style.left = -toLeft * rate + 'px';
+		   bimg.style.top = -toTop * rate + 'px';
+		
+	   }
+	   box.onmouseover = function (){
+		   // 鼠标移入时，显示滑块和大图
+		   move.style.display = "block";
+		   bimgDiv.style.display = "block";
+	   }
+	   box.onmouseout = function (){
+			// 鼠标移出时，隐藏滑块和大图
+		   move.style.display = "none";
+		   bimgDiv.style.display = "none";
+		   
+	   }
+	 //
+	 //点击按钮加index时间
+	 var index=0;
+	 var a = document.getElementById("a");
+	 var b = document.getElementById("b");
+	 var int1 = document.getElementById("in");
+	a.onclick = function(){
+		index++;
+		//console.log(index);
+		int1.innerHTML = index;
+	}
+	b.onclick = function(){
+		index--;
+		if(index < 0){
+			index = 0;
+			return;
+		}
+		int1.innerHTML = index;
+	}
+	console.log(index);
+	
+//	添加到购物车
+	
+//	添加到购物车
+
+
+	var a = document.getElementById('no1');
+	
+	//var img1 = document.getElementById('img1').src;
+	var img1 = $("#img1").attr('src');
+	//console.log(img1);
+	var inf = document.getElementById('inf');
+	var price = document.getElementById('price');
+	//console.log(price.innerHTML);
+	var int1 = document.getElementById("in");
+	//console.log(img1.src);
+	a.onclick = function(){
+		var username = '1'; //当前登录用户
+		var goodimg = img1;
+		var goodinf = inf.innerHTML;
+		var goodprice = price.innerHTML;
+		var goodnumber = int1.innerHTML;
+		
+		var cartstr = getCookie('cart') ? getCookie('cart'): '{}';
+		var cart = JSON.parse(cartstr);
+
+			cart[username] = {
+				img:goodimg,
+				info:goodinf,
+				//console.log(info);
+				price:goodprice,
+				num:goodnumber
+				
+			};
+		console.log(cart);
+		cartstr = JSON.stringify(cart);
+		// 存回cookie当中
+		setCookie('cart', cartstr, 7);
+		location.href = "../shopcar/shopcar.html"
+	}
+	
+}
+
---
+++ js/index.js
@@ -0,0 +1,216 @@
+﻿
+window.onload=function(){
+	//1.head部分二维码效果
+	var daling = document.getElementById('daling');
+	var daling1 = document.getElementById('daling1');
+	daling.onmouseover = function(){
+		daling1.style.display = "block";
+	}
+	daling.onmouseout = function(){
+		daling1.style.display = "none";
+	}
+	daling1.onmouseover = function(){
+		daling1.style.display = "block";
+	}
+	daling1.onmouseout = function(){
+		daling1.style.display = "none";
+	}
+	//head部分二维码效果
+	//11. 倒计时效果
+	function show(){
+		var time = document.getElementById('ti');
+		var time1 = document.getElementById('ti1');
+		var time2 = document.getElementById('ti2');
+		var time3 = document.getElementById('ti3');
+		var end = new Date(2016,08,19);
+		//月份从0开始；
+		var now = new Date();
+		var total = end.getTime() - now.getTime();
+		
+		var d = 24*60*60*1000;
+		var h = 60*60*1000;
+		var m = 60*1000;
+		var mis = 1000;
+		
+		var day = parseInt(total / d);
+		   //console.log(day);
+		var hou = parseInt((total % d)/h);
+		var min = parseInt((total % d % h ) / m);
+		var mins = parseInt((total % d % h % m ) / mis);
+		
+		var str="倒计时";
+		
+		str += adjust(day) + "天" + adjust(hou) + "时" + adjust(min)+ "分" + adjust(mins) + "秒";
+//		console.log(str);
+		time.innerHTML = str;
+		time1.innerHTML = str;
+		time2.innerHTML = str;
+		time3.innerHTML = str;
+	}		
+		show();
+		setInterval(show,1000);
+		
+		function adjust(num){
+			if(num < 10){
+				return "0"+num;
+			}	
+			return ""+num;
+		}
+	
+	//11. 倒计时效果
+	
+	//4.navindex部分效果
+	//轮播图效果
+	$(function(){
+		var $navli = $('.navt li');
+		var index = 0;
+		console.log($navli);
+		function letshow(){
+			$('.navb li').removeClass('select').eq(index).addClass('select');
+			$navli.eq(index).fadeIn(300).siblings().fadeOut(300);
+		}
+		letshow();
+		function current(){
+			index++;
+			if(index==$navli.size()){
+				index = 0;
+			}
+			letshow();
+		}
+		var timer = setInterval(current,2500);
+		$(".navb li").click(function(){
+			clearInterval(timer);
+			index = $(this).index();
+			letshow();
+			timer = setInterval(current,2500);
+		});
+	});
+	//轮播图
+	//侧拉框
+	$(function(){
+	//5图片变动效果
+		$('.but a').mouseover(function(){
+				$(this).children().css('position','relative').stop().animate({left:'-15px',top:'-15px',height:'300px'},300);	
+		});
+		$('.but a').mouseout(function(){
+				$(this).children().stop().animate({left:'0',top:'0',height:'283px'},300);
+		});
+	//5图片变动效果
+		$('.indeximg dl').mouseover(function(){
+			var _self=$(this);
+			$(this).addClass('hover').siblings().removeClass('hover');
+			$('.indeximg_n').css({'display':'block','border':'1px solid black'});
+				$('.indeximg_n').mouseover(function(){
+				$(this).css({'display':'block','border':'1px solid black'});
+				_self.addClass('hover').siblings().removeClass('hover');
+			});
+		});
+		$('.indeximg dl').mouseout(function(){
+			var _self = $(this);
+			
+			$(this).removeClass('hover');
+			
+			$('.indeximg_n').css({'display':'none','border':'1px solid black'});
+			
+				$('.indeximg_n').mouseout(function(){
+				$(this).css({'display':'none','border':'1px solid black'});
+				
+				_self.removeClass('hover');
+			});
+		});
+	//侧拉框
+	//7 效果改变
+		$('.world ul li').mouseover(function(){
+				$(this).children().children().css('position','relative').stop().animate({left:'-3px',top:'-1px',right:'3px',buttom:'1',width:'210',height:'143px'},300);	
+		});
+		$('.world ul li').mouseout(function(){
+				$(this).children().children().stop().animate({left:'0',top:'0',right:'0px',buttom:'0',width:'207px',height:'141px'},300);
+		});
+	//7 效果改变
+	//6 new 换组图
+			var aindex = 0;
+		$('.new_lr a').click(function(){
+			aindex++;
+		console.log(aindex);
+			if(aindex == 2){
+				aindex=-1;
+				//return aindex;
+			}
+				$('.new_ .new_s').eq(aindex).siblings().css('display','none');
+				$('.new_ .new_s').eq(aindex).css('display','block');
+				
+			
+		});
+	
+	//6 new 换组图
+	
+	//8 best换组图
+
+		$('.best_t li ').click(function(){
+			    //console.log($('.best_t li '));
+			$(this).children().css('background','#e14958');	
+			$(this).siblings().children().css('background','#fff');	
+			var index = $(this).index();
+			    //console.log(index);
+			$('.best_ div').eq(index).siblings().css('display','none');
+			
+			$('.best_ div').eq(index).css('display','block').siblings().css('display','none');
+			
+
+		});
+		
+		
+		
+	//8 best换组图	
+	//9 buy小变大的框
+		$('.buy .buy_body_l div').mouseenter(function(){
+			//console.log($('.buy .buy_body_l dl'));
+			var _self = this;
+			var $dv = $('.buy .buy_body_l div');
+			if($dv.children().css('display','block')){
+				$(_self).children().css('display','none').siblings().css('display','block');
+				return;
+			}
+			
+		});
+		$('.buy .buy_body_l div').mouseleave(function(){
+			var _self = this;
+			var $dv = $('.buy .buy_body_l div');
+			if($dv.children().css('display','none')){
+				$(_self).children().css('display','block').siblings().css('display','none');
+				return;
+			}
+		});
+	//9 buy小变大的框
+	});
+	
+	//navindex部分效果
+	
+	//忘记写了一个效果;
+	$("#l2").mouseenter(function(){
+		$(this).css({boxShadow:"#666 0px 0px 10px"});
+	});
+	$("#l2").mouseout(function(){
+		$(this).css({boxShadow:"none"});
+	});
+	$("#l3").mouseenter(function(){
+		$(this).css({boxShadow:"#666 0px 0px 10px"});
+	});
+	$("#l3").mouseout(function(){
+		$(this).css({boxShadow:"none"});
+	});
+	$("#l4").mouseenter(function(){
+		$(this).css({boxShadow:"#666 0px 0px 10px"});
+	});
+	$("#l4").mouseout(function(){
+		$(this).css({boxShadow:"none"});
+	});
+	$("#l5").mouseenter(function(){
+		$(this).css({boxShadow:"#666 0px 0px 10px"});
+	});
+	$("#l5").mouseout(function(){
+		$(this).css({boxShadow:"none"});
+	});
+	//忘记写了一个效果;
+}
+
